2b01d1c1a9b96f1cef454ff235edebbe3801b627
MulishoMusambyaThierry <102702310+Mulisho@users.noreply.github.com>
2025-03-24 10:12:04 +0200

    Add files via upload

---
+++ tp8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+      
+
+public class ConversionTemperature
+    {
+        public static void Main(string[] args)
+        {
+            // Déclarer les variables pour la température en Celsius et Fahrenheit
+            double celsius, fahrenheit;
+
+            // Demander à l'utilisateur d'entrer la température en Celsius
+            Console.Write("Entrez la température en degrés Celsius : ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            // Convertir Celsius en Fahrenheit
+            fahrenheit = (celsius * 1.8) + 32;
+
+            // Afficher le résultat
+            Console.WriteLine($"{celsius} degrés Celsius équivaut à {fahrenheit} degrés Fahrenheit.");
+        }
+    }
+}
+}
