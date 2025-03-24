754c6083b4f92effd376a41d09264205ab8e3393
MulishoMusambyaThierry <102702310+Mulisho@users.noreply.github.com>
2025-03-24 10:14:08 +0200

    Add files via upload

---
+++ TP29.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication29
+{
+    class Program
+    {
+      
+public class RacinesCarrées
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Entrez un nombre (0 pour arrêter) :");
+                double nombre = Convert.ToDouble(Console.ReadLine());
+
+                if (nombre == 0)
+                {
+                    break; // Sortir de la boucle si l'utilisateur entre 0
+                }
+                else if (nombre < 0)
+                {
+                    Console.WriteLine("Nombre négatif. La racine carrée n'est pas définie.");
+                }
+                else
+                {
+                    double racine = Math.Sqrt(nombre);
+                    Console.WriteLine($"La racine carrée de {nombre} est : {racine}");
+                }
+            }
+        }
+    }
+}
+}
---
+++ TP30.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication30
+{
+    class Program
+    {
+      
+public class AffichageISIG
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez le nombre de fois à afficher ISIG/GOMA :");
+            int nombreDeFois = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < nombreDeFois; i++)
+            {
+                Console.WriteLine("ISIG/GOMA");
+            }
+        }
+    }
+}
+}
